44b611ed589e89755d2d8ae3c819c1524bdf0fc1
ShadowYang <justiceofshadow@gmail.com>
2015-06-24 21:47:51 +0800

    Auto Minimized

--- Beta/MainWindow.xaml.cs
+++ Beta/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace Beta
 {
@@ -20,9 +21,83 @@ namespace Beta
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NotifyIcon notifyIcon;
         public MainWindow()
         {
             InitializeComponent();
+
+            init();
+            
+            
         }
+
+        public void init()
+        {
+            icon();
+            this.StateChanged += new EventHandler(Window_StateChanged);
+            this.Deactivated += new EventHandler(Window_LostFocus);
+        }
+
+        public void icon()
+        {
+            this.notifyIcon = new NotifyIcon();
+            this.notifyIcon.BalloonTipText = "Beta最小化中...";
+            this.notifyIcon.ShowBalloonTip(2000);
+            this.notifyIcon.Text = "Beta最小化中...";
+            this.notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
+            //this.notifyIcon.Icon = new System.Drawing.Icon(@"AppIcon.ico");
+            this.notifyIcon.Visible = true;
+            //打开菜单项
+            System.Windows.Forms.MenuItem open = new System.Windows.Forms.MenuItem("Open");
+            open.Click += new EventHandler(Show);
+            //退出菜单项
+            System.Windows.Forms.MenuItem exit = new System.Windows.Forms.MenuItem("Exit");
+            exit.Click += new EventHandler(Close);
+            //关联托盘控件
+            System.Windows.Forms.MenuItem[] childen = new System.Windows.Forms.MenuItem[] { open, exit };
+            notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu(childen);
+
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler((o, e) =>
+            {
+                if (e.Button == MouseButtons.Left) this.Show(o, e);
+            });
+        }
+
+        private void Window_LostFocus(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            WindowState ws = WindowState;
+            /*
+            if (ws == WindowState.Minimized)
+            {
+                Hide();
+            }
+            */
+        }
+
+        private void Show(object sender, EventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Visible;
+            this.ShowInTaskbar = true;
+            this.Activate();
+        }
+
+        private void Hide(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Close(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        
+
+            
     }
 }
